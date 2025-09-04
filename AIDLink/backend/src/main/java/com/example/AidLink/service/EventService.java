3d07c82a01a4503130ca327db5e4713fa3794ad0
LihanCanCode <tahsanlihan@iut-dhaka.edu>
2025-09-04 22:08:14 +0600

    Refactor EventService: auto-increment event id, use int for donationAmount, simplify logic

--- AIDLink/backend/src/main/java/com/example/AidLink/service/EventService.java
+++ AIDLink/backend/src/main/java/com/example/AidLink/service/EventService.java
@@ -19,35 +19,44 @@ public class EventService {
 		this.userRepository = userRepository;
 	}
 	// Update both event funding and user's donatedAmount
-	public Event updateEventFundingAndUserDonation(Integer eventId, Integer donationAmount, String userEmail) {
-		System.out.println("EventService: Updating event funding for event ID: " + eventId + " with amount: " + donationAmount);
-		Optional<Event> eventOptional = eventRepository.findById(eventId);
-		if (eventOptional.isPresent()) {
-			Event event = eventOptional.get();
-			Integer newCurrentFunding = (event.getCurrentFunding() != null ? event.getCurrentFunding() : 0) + (donationAmount != null ? donationAmount : 0);
-			event.setCurrentFunding(newCurrentFunding);
-			eventRepository.save(event);
+	   public Event updateEventFundingAndUserDonation(Integer eventId, int donationAmount, String userEmail) {
+		   System.out.println("EventService: Updating event funding for event ID: " + eventId + " with amount: " + donationAmount);
+		   Optional<Event> eventOptional = eventRepository.findById(eventId);
+		   if (eventOptional.isPresent()) {
+			   Event event = eventOptional.get();
+			   int newCurrentFunding = (event.getCurrentFunding() != null ? event.getCurrentFunding() : 0) + donationAmount;
+			   event.setCurrentFunding(newCurrentFunding);
+			   eventRepository.save(event);
 
-			// Update user's donatedAmount
-			User user = userRepository.findByEmail(userEmail);
-			if (user != null) {
-				int newDonatedAmount = user.getDonatedAmount() + (donationAmount != null ? donationAmount : 0);
-				user.setDonatedAmount(newDonatedAmount);
-				userRepository.save(user);
-				System.out.println("User " + userEmail + " donated. New donatedAmount: " + newDonatedAmount);
-			} else {
-				System.out.println("User not found with email: " + userEmail);
-			}
-			return event;
-		} else {
-			System.out.println("EventService: Event not found with ID: " + eventId);
-		}
-		return null;
-	}
+			   // Update user's donatedAmount
+			   User user = userRepository.findByEmail(userEmail);
+			   if (user != null) {
+				   int newDonatedAmount = user.getDonatedAmount() + donationAmount;
+				   user.setDonatedAmount(newDonatedAmount);
+				   userRepository.save(user);
+				   System.out.println("User " + userEmail + " donated. New donatedAmount: " + newDonatedAmount);
+			   } else {
+				   System.out.println("User not found with email: " + userEmail);
+			   }
+			   return event;
+		   } else {
+			   System.out.println("EventService: Event not found with ID: " + eventId);
+		   }
+		   return null;
+	   }
 
-	public Event addEvent(Event event) {
-		return eventRepository.save(event);
-	}
+	   public Event addEvent(Event event) {
+		   // If id is null, generate a new unique integer id
+		   if (event.getId() == null) {
+			   List<Event> allEvents = eventRepository.findAll();
+			   int maxId = allEvents.stream()
+				   .map(e -> e.getId() == null ? 0 : e.getId())
+				   .max(Integer::compareTo)
+				   .orElse(0);
+			   event.setId(maxId + 1);
+		   }
+		   return eventRepository.save(event);
+	   }
 
 	public List<Event> getAllEvents() {
 		return eventRepository.findAll();
@@ -57,24 +66,24 @@ public class EventService {
 		return eventRepository.findById(id);
 	}
 
-	public Event updateEventFunding(Integer eventId, Integer donationAmount) {
-		System.out.println("EventService: Updating event funding for event ID: " + eventId + " with amount: " + donationAmount);
+	   public Event updateEventFunding(Integer eventId, int donationAmount) {
+		   System.out.println("EventService: Updating event funding for event ID: " + eventId + " with amount: " + donationAmount);
 
-		Optional<Event> eventOptional = eventRepository.findById(eventId);
-		if (eventOptional.isPresent()) {
-			Event event = eventOptional.get();
-			System.out.println("EventService: Found event: " + event.getId() + " - " + event.getTitle());
-			System.out.println("EventService: Current funding: " + event.getCurrentFunding() + ", Funding goal: " + event.getFundingGoal());
+		   Optional<Event> eventOptional = eventRepository.findById(eventId);
+		   if (eventOptional.isPresent()) {
+			   Event event = eventOptional.get();
+			   System.out.println("EventService: Found event: " + event.getId() + " - " + event.getTitle());
+			   System.out.println("EventService: Current funding: " + event.getCurrentFunding() + ", Funding goal: " + event.getFundingGoal());
 
-			// Add the donation amount to the current funding
-			Integer newCurrentFunding = (event.getCurrentFunding() != null ? event.getCurrentFunding() : 0) + (donationAmount != null ? donationAmount : 0);
-			event.setCurrentFunding(newCurrentFunding);
+			   // Add the donation amount to the current funding
+			   int newCurrentFunding = (event.getCurrentFunding() != null ? event.getCurrentFunding() : 0) + donationAmount;
+			   event.setCurrentFunding(newCurrentFunding);
 
-			System.out.println("EventService: New funding amount: " + newCurrentFunding);
-			return eventRepository.save(event);
-		} else {
-			System.out.println("EventService: Event not found with ID: " + eventId);
-		}
-		return null;
-	}
+			   System.out.println("EventService: New funding amount: " + newCurrentFunding);
+			   return eventRepository.save(event);
+		   } else {
+			   System.out.println("EventService: Event not found with ID: " + eventId);
+		   }
+		   return null;
+	   }
 }
